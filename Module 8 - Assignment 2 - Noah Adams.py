5be94849b1b40d7cefead148028c4bff9cb86880
Noah Adams <78328941+Insane-Owl@users.noreply.github.com>
2025-05-05 15:58:48 -0400

    Add files via upload

---
+++ Module 8 - Assignment 2 - Noah Adams.py
@@ -0,0 +1,7 @@
+def greater_than(x, y):
+    return x > y # this will return true if x is greater than y, and false if not
+
+a = 10
+b = 6
+result = greater_than(a, b)
+print(f"The statement %s is greater than %s is %s" % (a, b, result))
